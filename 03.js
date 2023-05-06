b376f7f1e6f5f5ffb868f7996f96287a1b763147
hjun0917 <hjun0917@naver.com>
2023-05-06 15:45:07 +0900

    데이터 타입(2) - 문자열

--- 03.js
+++ 03.js
@@ -20,4 +20,14 @@ let str3 = "Hello, World!";
 
 // 2-4. 문자열 검색
 let str4 = "Hello, World";
-console.log(str4.search("World"));
+// console.log(str4.search("World"));
+
+// 2-5. 문자열 대체
+let str5 = "Hello World!";
+let result01 = str5.replace("World!", "JavaScript");
+// console.log(result01);
+
+// 2-6. 문자열 분할
+let str6 = "apple, banana, watermelon";
+let result02 = str6.split(",");
+console.log(result02);
