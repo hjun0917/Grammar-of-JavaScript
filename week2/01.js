3be8898a3e4803f580e558dfadd3e009362f0c17
hjun0917 <hjun0917@naver.com>
2023-07-23 10:31:50 +0900

    변수, 함수, 삼항 연산자 복습

--- week2/01.js
+++ week2/01.js
@@ -3,8 +3,41 @@
 */
 
 // 2015년도 이전 -> var
-// es6 -> let(변수), const(상수)
+// (1) es6 -> let(변수), const(상수)
 const a = 1;
-a = 2;
+// a = 2; 재할당 x
+
+let b = 1;
+b = 2;
+// let b = 3; 재선언 x
+
+var c = 1;
+c = 2; // 재할당 o
+var c = 3; // 재선언 o
 
 console.log(a);
+console.log(b);
+console.log(c);
+
+// (2) arrow function
+
+// 함수 선언문
+function add () {
+
+}
+
+// 함수 표현식
+var add = function () {
+
+}
+
+var add = () => {
+    return 1;
+}
+
+var add = () => 1;
+
+var add = x => 1;
+
+// (3) 삼항 연산자
+// condition ? true인 경우 : false인 경우;
