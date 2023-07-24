b389c6ba1c61a92ca9c11002711bb41958f0ec1d
hjun0917 <hjun0917@naver.com>
2023-07-25 01:07:17 +0900

    구조분해할당

--- week2/02.js
+++ week2/02.js
@@ -4,32 +4,39 @@
 // (1) 배열
 let [value1, value2] = [1, "new"];
 console.log("1->", value1);
-let arr = [1, 2, 3];
-let [a, b, c, d] = arr;
-console.log(c);
-console.log(d); // undefined, null은 보통 개발자가 명시적으로 없다는 것을 표현했을 경우
-// let [a, b, c, d =4] = arr; -> 해당과 같이 초기값을 지정해줄 수 있음.
+console.log("2->", value2);
+
+// let arr = [1, 2, 3];
+let arr = [1, 2, 3, 4];
+// let [a, b, c] = arr;
+let [a, b, c, d = 10] = arr; // 해당과 같이 초기값을 지정해줄 수 있음.
+console.log("c->", c);
+console.log("d->", d); // undefined, null은 보통 개발자가 명시적으로 없다는 것을 표현했을 경우
 
 // (2) 객체
-let person = {
-    name : "a",
-    age : 20
-}
+let user = {
+    name: "abc",
+    age: 30,
+    birthday: "yesterday",
+};
 
-let {name, age} = {
-    name : "a",
-    age : 30
-}
-console.log(person.name);
-console.log("이름 :",name, ", 나이 :",age);
+// let { name, age } = {
+//     name: "abc",
+//     age: 30,
+// };
 
-// 새로운 이름으로 할당
-let {
-    name : newName,
-    age : newAge
-} = person;
+// console.log(name);
+// console.log(typeof name);
+// console.log(age);
+// console.log(typeof age);
+
+// let { name: newName, age: newAge } = user;
+
+// console.log(newName);
+// console.log(newAge);
+
+let { name, age, birthday = "today" } = user;
 
 console.log(name);
-console.log(newName);
-
-console.log(person);
+console.log(age);
+console.log(birthday);
