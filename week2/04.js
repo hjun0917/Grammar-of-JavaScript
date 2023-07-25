5ccebfccc3ee33f301740729e14aea3a5cf2e2cf
hjun0917 <hjun0917@naver.com>
2023-07-25 16:05:44 +0900

    일급객체로써의 함수(1)

---
+++ week2/04.js
@@ -0,0 +1,36 @@
+/**
+ * 자바스크립트에서 함수는 일급 객체(first-class object)라고 한다.
+ * 일급 객체란 다른 객체들에 일반적으로 적용 가능한 연산을 모두 지원하는 객체를 가리킨다.
+ */
+
+// (1) 변수에 함수를 할당
+// 함수가 마치 값으로 취급된다.
+// 함수가 나중에 사용될 수 있도록 조치가 되었다.
+const sayHello = function () {
+    console.log("Hello");
+};
+
+// (2) 함수를 인자로 다른 함수에 전달할 수가 있다.
+// (2)-1. 콜백함수 : 매개변수로써 쓰이는 함수
+// (2)-2. 고차함수 : 함수를 인자로 받거나 return하는 함수
+function callFunction(func) {
+    // 매개변수로 받은 변수가 사실, 함수다.
+    func();
+}
+
+console.log(callFunction(sayHello));
+
+// (3) 함수를 반환할 수 있다.
+function createAdder(num) {
+    return function (x) {
+        return x + num;
+    };
+}
+
+const addFive = createAdder(5);
+/**
+ * const addFive = function (x) {
+        return x + num;
+    };
+ */
+console.log(addFive(10));
