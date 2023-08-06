ded9fce5d0457ae8f9b8041d53302dad93a84d52
hjun0917 <hjun0917@naver.com>
2023-08-06 18:48:54 +0900

    NaN 실습

---
+++ week1/prac.js
@@ -0,0 +1,9 @@
+// var a = nan; nan is not defined -> 식별자로 인식한다.
+// var b = Nan; Nan is not defined
+// var c = NAN; NAN is not defined
+var d = NaN;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
